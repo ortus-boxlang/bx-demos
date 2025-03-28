dc098a6fb0fcb30a783956c0ad28062ecd59eb04
Raymond Camden <raymondcamden@gmail.com>
2025-03-28 12:21:42 -0500

    speech stuff

--- webapps/grumpycat/app.js
+++ webapps/grumpycat/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
 let $userChat, $sendChat, $chatResult;
+let voices;
 
 async function init() {
 	console.log('init');
@@ -8,6 +9,7 @@ async function init() {
 	$sendChat = document.querySelector('#sendChat');
 	$chatResult = document.querySelector('#chatResult');
 	$sendChat.addEventListener('click', sendMessage, false);
+
 }
 
 async function sendMessage() {
@@ -21,6 +23,34 @@ async function sendMessage() {
 	// add the sleep here
 	await sleep(2);
 	$chatResult.innerHTML = result;
+	/*
+	Due to an issue w/ how getVoices work, we can't do this in init().
+	https://stackoverflow.com/a/52005323/52160
+	*/
+	/*
+	let voices = window.speechSynthesis.getVoices();
+	console.log(voices);
+	for(v of voices) {
+		console.log(v);
+	}
+	*/
+
+	speechSynthesis.cancel();
+	let utterance = new SpeechSynthesisUtterance(result);
+
+	if(!voices) voices = await getVoices();
+
+	for(v of voices) {
+		if(v.name === 'Google UK English Male') {
+			console.log('SET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+			utterance.voice = v;
+			console.log(v);
+		}
+	}
+
+	console.log('now going to speak');
+	speechSynthesis.speak(utterance);
+
 	$userChat.value = '';
 	$sendChat.disabled = false;
 }
@@ -30,4 +60,20 @@ Because BoxLang + Gemini is so dang quick, I need an artificial slowdown
 */
 async function sleep(seconds) {
 	return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
+async function getVoices() {
+    return new Promise(
+        function (resolve, reject) {
+            let synth = window.speechSynthesis;
+            let id;
+
+            id = setInterval(() => {
+                if (synth.getVoices().length !== 0) {
+                    resolve(synth.getVoices());
+                    clearInterval(id);
+                }
+            }, 10);
+        }
+    )
 }
