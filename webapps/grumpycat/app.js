f98d00166718d8f595227d31c66280c25571428d
Raymond Camden <raymondcamden@gmail.com>
2025-03-28 08:23:26 -0500

    new demo

---
+++ webapps/grumpycat/app.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', init, false);
+
+let $userChat, $sendChat, $chatResult;
+
+async function init() {
+	console.log('init');
+	$userChat = document.querySelector('#userChat');
+	$sendChat = document.querySelector('#sendChat');
+	$chatResult = document.querySelector('#chatResult');
+	$sendChat.addEventListener('click', sendMessage, false);
+}
+
+async function sendMessage() {
+	let input = $userChat.value.trim();
+	if(input === '') return;
+	$sendChat.disabled = true;
+	$chatResult.innerHTML = '<p><img src="/grumpy_cat.png" class="rotating-image"></p>';
+	console.log('sending ', input);
+	let req = await fetch(`./ai.bx?method=processChat&input=${encodeURIComponent(input)}`);
+	let result = await req.json();
+	// add the sleep here
+	await sleep(2);
+	$chatResult.innerHTML = result;
+	$userChat.value = '';
+	$sendChat.disabled = false;
+}
+
+/*
+Because BoxLang + Gemini is so dang quick, I need an artificial slowdown
+*/
+async function sleep(seconds) {
+	return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
