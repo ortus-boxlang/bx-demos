c23f1b3868cf3c1a7b4734d14e902ef4cc77a853
Raymond Camden <raymondcamden@gmail.com>
2025-08-20 09:09:54 -0500

    misc

--- webapps/logviewer/app.js
+++ webapps/logviewer/app.js
@@ -4,19 +4,14 @@ document.addEventListener('alpine:init', () => {
 	Alpine.data('app', () => ({
 		filter:'',
 		origlogcontents:'',
-		logcontents:'',
 		init() {
 			console.log('alpine init');
-			this.logcontents = this.$refs.logTextArea.value;
-			this.origlogcontents = this.logcontents;
+			this.origlogcontents = this.$refs.logTextArea.value;
 			
 			this.$watch('filter', (curr) => {
 				if(curr == '') {
-					this.logcontents = this.origlogcontents;
-					return;
-				}
-
-				this.logcontents = this.origlogcontents.split('\n').filter(l => l.indexOf(curr) >= 0).join('\n');
+					this.$refs.logTextArea.value = this.origlogcontents;
+				} else this.$refs.logTextArea.value = this.origlogcontents.split('\n').filter(l => l.indexOf(curr) >= 0).join('\n');
 			});
 			
 		}
